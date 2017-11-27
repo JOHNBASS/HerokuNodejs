8bb394013744b3b6722951ab766b27cf0bf7b3b0
John C Chang (RD-TW) <john_c_chang@tw-johncchang.client.tw.trendnet.org>
2017-11-27 18:52:37 +0800

    upadeta

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var app = express();
+var webdriver = require('selenium-webdriver');
+
+var By = webdriver.By;
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -23,6 +26,27 @@ app.get('/favicon.ico', (req, res) => {
   res.status(204);
 });
 
+//Applications
+
+app.get('/test', function (req, res) {
+    var driver = new webdriver.Builder()
+        .forBrowser('phantomjs')
+        .build();
+    driver.get('http://www.google.com/ncr');
+    driver.findElement(By.name('q')).sendKeys('webdriver');
+    driver.findElement(By.name('btnG')).click();
+    driver.wait(function() {
+        return driver.getTitle().then(function(title) {
+            console.log(title);
+            return title === 'webdriver - Google Search';
+        });
+    }, 5000).then(function() {
+        res.status(200).send('Done');
+    }, function(error) {
+        res.status(200).send(error);
+    });
+    driver.quit();
+});
 
 
 app.listen(app.get('port'), function() {
